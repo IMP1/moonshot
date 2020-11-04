2264af0e96e2c580a7905bf10ada8a025c31557e
IMP1 <huw_taylor@hotmail.co.uk>
2020-11-04 19:20:36 +0000

    Add placeholder images and config file

---
+++ conf.lua
@@ -0,0 +1,12 @@
+function love.conf(game)
+	game.window.title = "Moon Shots"
+	game.window.width = 640
+	game.window.height = 480
+
+    game.modules.data = false
+    game.modules.joystick = false
+    game.modules.math = false
+    game.modules.physics = false
+    game.modules.thread = false
+    game.modules.video = false
+end
